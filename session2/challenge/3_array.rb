14baf1dc743995111ce34b36c716d2e7977c339f
Sophia Bell <Kittaru87@gmail.com>
2020-02-14 18:13:48 +0000

    completed session2 challenge

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -7,5 +7,9 @@
 
 class String
   def every_other_char
+    empty_array = []
+    letter_array = self.chars
+    letter_array.each_with_index{|letter, index| empty_array << letter if index.even?}
+    empty_array.join("")
   end
 end
