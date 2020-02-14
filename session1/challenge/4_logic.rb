3499cbf73390b9a328a7cd089f6871922395ec3e
Sophia Bell <Kittaru87@gmail.com>
2020-02-14 17:58:09 +0000

    passed session 1 tests

--- session1/challenge/4_logic.rb
+++ session1/challenge/4_logic.rb
@@ -1,6 +1,6 @@
 # A grad student at a local university thinks he has discovered a formula to
-# predict what kind of grades a person will get. He says if you own less than 
-# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C", 
+# predict what kind of grades a person will get. He says if you own less than
+# 10 books, you will get a "D". If you own 10 to 20 books, you will get a "C",
 # and if you own more than 20 books, you will get a "B".
 # He further hypothesizes that if you actually read your books, then you will
 # get a full letter grade higher in every case.
@@ -10,4 +10,15 @@
 # grade(15, true)   # => "B"
 
 def grade(num_books, reads_books)
+  grades = ["A", "B", "C", "D"]
+
+  if reads_books == false
+    return grades[3] if num_books < 10
+    return grades[2] if num_books < 21
+    return grades[1] if num_books > 20
+  else
+    return grades[2] if num_books < 10
+    return grades[1] if num_books < 21
+    return grades[0] if num_books > 20
+  end
 end
--- session1/challenge/6_string.rb
+++ session1/challenge/6_string.rb
@@ -1,11 +1,22 @@
 # You'll get a string and a boolean.
 # When the boolean is true, return a new string containing all the odd characters.
 # When the boolean is false, return a new string containing all the even characters.
-# 
+#
 # If you have no idea where to begin, remember to check out the cheatsheets for string and logic/control
-# 
+#
 # odds_and_evens("abcdefg",true)    # => "bdf"
 # odds_and_evens("abcdefg",false)   # => "aceg"
 
 def odds_and_evens(string, return_odds)
+  empty_array = []
+  s_array = string.chars
+
+  if return_odds == true
+    s_array.each_with_index {|letter, index| empty_array << letter if index.odd?}
+  else
+    s_array.each_with_index {|letter, index| empty_array << letter if index.even?}
+  end
+
+  empty_array.join("")
+
 end
